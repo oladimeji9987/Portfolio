44ada7349f85ddaee94e499d998eef33b9beafd9
Ismaheel <oladimeji2312@gmail.com>
2024-11-10 14:48:29 +0100

    Initial commit

---
+++ Portfolio/src/assets/components/Header.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom"
+const Header = ()=>{
+    return (
+        <div className="navBar">
+            <h2>ISMAHEEL OLADIMEJI</h2>
+
+            <Link to="/Home">
+                <h4>Home</h4>
+            </Link>
+
+            <Link to="/About">
+                <h4>About</h4>
+            </Link>
+
+            <Link to="/Capability">
+                <h4>Capability</h4>
+            </Link>
+
+            <Link to="/Project">
+                <h4>Project</h4>
+            </Link>
+
+            <Link to="/Contact">
+                <h4>Contact</h4>
+            </Link>
+        </div>
+    )
+}
+
+export default Header;
